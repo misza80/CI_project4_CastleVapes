0d626aaa6577ac32ef14b0703f04c4a0c9e64fd5
misza80 <55979600+misza80@users.noreply.github.com>
2020-09-25 21:43:07 +0100

    added card payment element and styling,added initial stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HVLnqF62i6weKxEBOqVPlaX4XMe3K47zg7Cl8c1YYotdOmC9Tl3jP47oAJ4PDoJv7WpuuYGcD7s8iwRYB7UhLuR0092yoRejR',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
