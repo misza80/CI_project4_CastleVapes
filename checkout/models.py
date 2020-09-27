410038de2399476186edd406af8ac46e9a1ee160
misza80 <55979600+misza80@users.noreply.github.com>
2020-09-27 19:56:22 +0100

    added order information to user profile

--- checkout/models.py
+++ checkout/models.py
@@ -3,9 +3,8 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-
-from products.models import Product
 from profiles.models import UserProfile
+from products.models import Product
 
 
 class Order(models.Model):
