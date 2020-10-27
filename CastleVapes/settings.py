52c70a016019a3395bebe187208a5d8b240246d3
misza80 <55979600+misza80@users.noreply.github.com>
2020-10-27 18:38:52 +0000

    testing for gmail acc verification

--- CastleVapes/settings.py
+++ CastleVapes/settings.py
@@ -214,14 +214,10 @@ if 'USE_AWS' in os.environ:
     STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
 
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'castlevapes@example.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+ EMAIL_USE_TLS = True
+ EMAIL_PORT = 587
+ EMAIL_HOST = 'smtp.gmail.com'
+ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+ DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
